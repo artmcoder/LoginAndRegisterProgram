aa5b9dfadb702ed7a4bb43142879320a93953231
Artem Yakunin <artm.yakunin@gmail.com>
2021-05-23 17:59:16 +0700

    final commit in dao class branch

--- src/com/loginandregister/LoginAndRegister.java
+++ src/com/loginandregister/LoginAndRegister.java
@@ -6,31 +6,65 @@ import com.loginandregister.model.User;
 import java.util.Scanner;
 
 public class LoginAndRegister {
-
+    public static final String POSSIBLE_ANSWER_1 = "register";
+    public static final String POSSIBLE_ANSWER_2 = "login";
+    public static final String POSSIBLE_ANSWER_3 = "show db";
 
     public static void main(String[] args) {
         UserDAO userDAO = new UserDAO();
         Scanner scanner = new Scanner(System.in);
         String login;
         String password;
+        String command;
         while (true) {
-            System.out.println("Register new user" +
-                    "\nPlease write your login and password");
-            System.out.print("Login: ");
-            login = scanner.nextLine();
-            System.out.print("Password: ");
-            password = scanner.nextLine();
-            User user = new User();
-            user.setLogin(login);
-            user.setPassword(password);
-            userDAO.registerUser(user);
-            System.out.println("User register successfully");
-            System.out.println("User db: ");
-            for (User u : userDAO.getUsersDb()) {
-                if (u == null) {
+            System.out.println("--------------------------------------------------");
+            System.out.println("Welcome! If do you want to login,\n" +
+                    "please write 'login' and \nif you want to register," +
+                    " write 'register'. Show user database: 'show db'");
+            System.out.print("Write command: ");
+            command = scanner.nextLine();
+            switch (command) {
+                case POSSIBLE_ANSWER_1:
+                    User user = new User();
+                    System.out.println("Ok, I understand you, want to register.\n"
+                    + "We need you login and password");
+                    System.out.print("Login: ");
+                    login = scanner.nextLine();
+                    System.out.print("Password: ");
+                    password = scanner.nextLine();
+                    user.setLogin(login);
+                    user.setPassword(password);
+                    if (userDAO.registerUser(user)) {
+                        System.out.println("Cool! User with login " + login +
+                                " was register");
+                    } else {
+                        System.out.println("Error! User with login " + login +
+                                " is exist");
+                    }
                     break;
-                }
-                System.out.println(u);
+                case POSSIBLE_ANSWER_2:
+                    System.out.println("Ok, I understand you, you want to login.\n"
+                    + "We need you login and password");
+                    System.out.print("Login: ");
+                    login = scanner.nextLine();
+                    System.out.print("Password: ");
+                    password = scanner.nextLine();
+                    if (userDAO.loginUser(login, password)) {
+                        System.out.println("Cool! User with login " + login +
+                                " was login in his personal area");
+                    } else {
+                        System.out.println("Error! Login or password is incorrect");
+                    }
+                    break;
+                case POSSIBLE_ANSWER_3:
+                    System.out.println("User database:");
+                    for (int i = 0; i < userDAO.getUsersDb().length; i++) {
+                        if (userDAO.getUsersDb()[i] == null) break;
+                        System.out.println(userDAO.getUsersDb()[i]);
+                    }
+                    break;
+                default:
+                    System.out.println("I not understand you. Can do this again");
             }
         }
     }
--- src/com/loginandregister/dao/UserDAO.java
+++ src/com/loginandregister/dao/UserDAO.java
@@ -6,15 +6,51 @@ public class UserDAO {
     private User[] usersDb = new User[100];
     private static int USER_ID = -1;
 
-    public void registerUser(User user) {
+    public boolean registerUser(User user) {
+        for (User u : usersDb) {
+            if (u == null) break;
+            if (u.getLogin().equals(user.getLogin())) {
+                return false;
+            }
+        }
         user.setId(++USER_ID);
         usersDb[USER_ID] = user;
-        System.out.println("Saving new User: "
-                + user);
+        return true;
     }
+
+    public boolean loginUser(String login, String password) {
+        User user = findUserByLogin(login);
+        if (user != null) {
+             if (correctPasswordFromUser(user, password)) {
+                return true;
+             } else {
+                 return false;
+             }
+        } else {
+            return false;
+        }
+    }
+
+    private boolean correctPasswordFromUser(User user, String password) {
+        if (user.getPassword().equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    private User findUserByLogin(String login) {
+        for (User u : usersDb) {
+            if (u == null) break;
+            if (u.getLogin().equals(login)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+
 
     public User[] getUsersDb() {
         return usersDb;
     }
-
 }
